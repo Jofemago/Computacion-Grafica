013956286f8fef9a563fefeddd3cdfb0db252819
Jofemago <jofemago@utp.edu.co>
2017-10-18 15:55:40 -0500

    falta poner plataforma

--- gravedad/main.py
+++ gravedad/main.py
@@ -12,13 +12,18 @@ if __name__ =='__main__':
     jugadores = pygame.sprite.Group()
     general = pygame.sprite.Group()
 
-
+    #pl = plataforma()
+    #pl.rect.x = 300
+    #pl.rect.y= 450
+    fondo = pygame.image.load('fondo.jpg')
+    f_x = 0
     jg = Player(50,50)
     #jg.rect.x = 0
     #jg.rect.y = 0
 
     jugadores.add(jg)
     general.add(jg)
+    #general.add(pl)
 
     reloj = pygame.time.Clock()
     fin  = False
@@ -27,11 +32,6 @@ if __name__ =='__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 fin=True
-
-            if event.type == pygame.KEYUP:
-
-                jg.var_y = 0
-                jg.var_x = 0
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
@@ -42,9 +42,26 @@ if __name__ =='__main__':
 
                     jg.var_x = -5
 
-        pantalla.fill(NEGRO)
+                if event.key == pygame.K_UP:
+                    #jg.gritar()
+                    jg.salto()
+                    #jg.var_y = -10
 
+            if event.type == pygame.KEYUP:
+
+                #jg.var_y = 0
+                if event.key == pygame.K_RIGHT and not event.key == pygame.K_LEFT:
+                    jg.var_x = 0
+                if event.key == pygame.K_LEFT and not event.key == pygame.K_RIGHT  :
+
+                    jg.var_x = 0
+
+
+
+        pantalla.fill(NEGRO)
+        pantalla.blit(fondo,[f_x,0])
         general.update()
         general.draw(pantalla)
         pygame.display.flip()
         reloj.tick(60)
+        f_x -= 3
--- gravedad/player.py
+++ gravedad/player.py
@@ -13,6 +13,15 @@ class Vida(pygame.sprite.Sprite):
         self.image = pygame.Surface([30,30])
         self.image.fill(VERDE)
         self.rect= self.image.get_rect()
+class plataforma(pygame.sprite.Sprite):
+    cl = ROJO
+    al = 200
+    an = 10
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([self.al,self.an])
+        self.image.fill(self.cl)
+        self.rect= self.image.get_rect()
 
 class Player(pygame.sprite.Sprite):
 
@@ -24,6 +33,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.setPos(0,0)
 
+        self.sonido = pygame.mixer.Sound('grito.ogg')
         #variables de movimiento
         self.var_x = 0
         self.var_y = 0
@@ -56,19 +66,41 @@ class Player(pygame.sprite.Sprite):
     def movY(self):
 
 
-        if self.rect.y >= ALTO -60 and self.var_y >= 0:
+        if self.rect.y >= ALTO - 60 and self.var_y >= 0:
             self.var_y = 0
 
         if self.rect.y <= 100 and self.var_y <= 0:
             self.var_y = 0
 
+
+
+
         self.rect.y += self.var_y
 
-    def update(self):
+
+    def gravedad(self):
 
         if self.var_y == 0:
             self.var_y = 1
         else:
-            self.var_y += 0.35
+            self.var_y += 0.25
+
+        if self.rect.y >= ALTO - self.rect.height and self.var_y >= 0:
+            self.var_y = 0
+            self.rect.y = ALTO - self.rect.height
+
+    def salto(self):
+        self.var_y = -10
+
+    def gritar(self):
+        self.sonido.play()
+
+    def update(self):
+
+
+
+        self.gravedad()
         self.movX()
-        self.movY()
+        self.rect.y += self.var_y
+
+        #self.movY()
